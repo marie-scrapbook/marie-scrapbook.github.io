91486ee4f825f0b844e576cdd4f6a11b9b81ccca
tarinrickett <trickett96@gmail.com>
2020-11-28 14:49:13 -0800

    Dummy Mobile Hamburger Menu
    - Set up markup and styles to accomodate a hamburger menu for mobile screens only
    - JS integration TBD

--- src/shared/Nav.js
+++ src/shared/Nav.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './Nav.css';
 import logo from './assets/signature.png';
+import hamburgerMenuIcon from './assets/hamburger-menu.png';
 
 function Nav() {
   return (
     <nav className="nav sticky-top scrap-bkg">
-      <a className="nav-logo-link" href="index.html">
-        <img className="nav-logo" src={logo} alt="Marie Tracy"/>
-      </a>
+      <div className="nav-main-row">
+        <a className="nav-logo-link" href="index.html">
+          <img className="nav-logo" src={logo} alt="Marie Tracy"/>
+        </a>
+        <img className="nav-hamburger-menu" id="nav-hamburger-menu" src={hamburgerMenuIcon} alt="hamburger menu by Dreshad Williams from the Noun Project"/>
+      </div>
       <a className="nav-link" href="index.html">Home</a>
       <a className="nav-link" href="index.html#timeline">Timeline</a>
       <a className="nav-link" href="index.html">Scrapbook</a>
