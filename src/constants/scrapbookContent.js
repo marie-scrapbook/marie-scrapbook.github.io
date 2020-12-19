1dc6c640abc8ede2fd02c3b262b9ab0c00d8b34f
tarinrickett <trickett96@gmail.com>
2020-12-19 13:51:53 -0800

    Add dummy content for remaining "chapters"
    - Plus fix missing set `width` for scrapbook pages

--- src/constants/scrapbookContent.js
+++ src/constants/scrapbookContent.js
@@ -1,9 +1,14 @@
 const img = {
+  'amesSisters': 'timeline/ames-sisters.jpg',
+  'graduation': 'timeline/graduation.jpeg',
+  'marvelettes': 'timeline/marvelettes-pyramid.png',
   'millerSisters': 'scrapbook/miller-sisters.png',
   'millerSistersRecital': 'timeline/miller-sisters-recital.jpg',
-  'tracySisters': 'timeline/tracy-sister-recital.jpg',
   'nedWayburn': 'scrapbook/ned-wayburn.jpg',
-  'graduation': 'timeline/graduation.jpeg'
+  'retirement': 'timeline/bar-across-the-street.png',
+  'roxyette': 'timeline/roxyette-ball.png',
+  'tracySisters': 'timeline/tracy-sister-recital.jpg',
+  'weddingCard': 'timeline/wedding-card.jpg'
 }
 
 const audio = {
@@ -47,6 +52,14 @@ export const scrapbookContent = [
       "Oh, this is my dancing school... My mother made most of the costumes. That's me, the teacher. We were doing one of the fairy tales, as you can see. This is Ann, my sister Ann. Patricia was the pianist. This is our backyard in Elmhurst. Ann tap danced but... she knew everything, she was a pain in the neck so we threw her out. I don't think she liked it. I wound up with like 100. It snowballed. Because it was 25¢ a lesson. When I look back now, it must have hurt the Miller Sisters. This was a time when Shirley Temple was starting to come up. Everyone wanted a Shirley Temple."
   },
   {
+    "chapter": "amesSisters",
+    "date": "March 1939",
+    "imgSrc": img.amesSisters,
+    "title": "On the road",
+    "transcription":
+      "I did two years with the Ames Sisters and the Marvelettes. I was on the road for two years.",
+  },
+  {
     "audioSrc": audio.graduation,
     "chapter": "graduation",
     "date": "June 1940",
@@ -54,5 +67,37 @@ export const scrapbookContent = [
     "title": "Newtown High School Class of 1940",
     "transcription": 
       "During high school I was doing my homework at night while we were working the shows. I had my own dancing school on Saturdays, and then we would get gigs. The Masons would have a big banquet and they'd hire a couple of acts, or Jewish weddings would have a couple of acts..."
+  },
+  {
+    "chapter": "marvelettes",
+    "date": "1940",
+    "imgSrc": img.marvelettes,
+    "title": "On the road",
+    "transcription":
+      "I did two years with the Ames Sisters and the Marvelettes. I was on the road for two years.",
+  },
+  {
+    "chapter": "roxyette",
+    "date": "1942",
+    "imgSrc": img.roxyette,
+    "title": "Life at the Roxy",
+    "transcription":
+      "Working with Gae Foster",
+  },
+  {
+    "chapter": "retirement",
+    "date": "1942",
+    "imgSrc": img.retirement,
+    "title": "Purple Heart Recipient",
+    "transcription":
+      "Girls made me a purple heart for being injured in the line of duty",
+  },
+  {
+    "chapter": "wedding",
+    "date": "1947",
+    "imgSrc": img.weddingCard,
+    "title": "From all of us",
+    "transcription":
+      "Wedding shower gift from the girls",
   },
 ];
--- src/constants/timelineContent.js
+++ src/constants/timelineContent.js
@@ -49,6 +49,7 @@ export const timelineContent = [
     "navYear": "1937",
   },
   {
+    "chapterIndex": getChapterIndex("amesSisters"),
     "date": "Mar 1939",
     "description": "Took over as the fifth Ames Sister after one of the real sisters became pregnant. At 17, toured from New England to Chicago in this singing, dancing, and acrobatic act.",
     "imgSrc": amesSisters,
@@ -63,6 +64,7 @@ export const timelineContent = [
     "title": formattedChapters["graduation"],
   },
   {
+    "chapterIndex": getChapterIndex("marvelettes"),
     "date": "1940-1941",
     "description": "As several of the Ames sisters leave to get married, the group brings in new girls and expands, now calling themselves the Marvelettes.",
     "imgSrc": marvelettes,
@@ -70,24 +72,28 @@ export const timelineContent = [
     "navYear": "1940",
   },
   {
+    "chapterIndex": getChapterIndex("marvelettes"),
     "date": "Aug 1941",
     "description": "After years of performing together, Marie declines the offer to join the Marvelettes on tour in South America.",
     "imgSrc": southAmerica,
     "title": '"No" to South America',
   },
   {
+    "chapterIndex": getChapterIndex("roxyette"),
     "date": "1942",
     "description": 'Marie auditions at both the Roxy and Radio City Music Hall, the first glimpse at the opportunity to fulfill her dream of being "one or the other"– a Rockette or Roxyette.',
     "imgSrc": oneOrTheOther,
     "title": '"One or the other"',
   },
   {
+    "chapterIndex": getChapterIndex("roxyette"),
     "date": "Feb 1942",
     "description": "A month after her audition, a spot opens up at the Roxy. Marie lands the gig and fulfills her childhood dream.",
     "imgSrc": showUpAtRoxy,
     "title": "Show up at the Roxy",
   },
   {
+    "chapterIndex": getChapterIndex("roxyette"),
     "date": "1942-1946",
     "description": "As a full-time dancer, Marie performed twice-a-day, seven days a week.",
     "imgSrc": roxyette,
@@ -95,12 +101,14 @@ export const timelineContent = [
     "navYear": "1942",
   },
   {
+    "chapterIndex": getChapterIndex("roxyette"),
     "date": "Sept 1945",
     "description": "In addition to rehersals and performances, Marie also acts the official fit model for the Roxy costumes. She joins the IATSE Labor Union as part of her expanded role.",
     "imgSrc": fitModel,
     "title": "Fit Model",
   },
   {
+    "chapterIndex": getChapterIndex("retirement"),
     "date": "Oct 1946",
     "description": "Marie falls ill with mastoiditis– a serious complication from an ear infection. The infection forces her into retirement. \nAlthough it may have felt like an early retirement, few lasted as long as Marie did at the Roxy, due to the rigorous standards for the dancers as well as societal pressure on the girls.",
     "imgSrc": retirementParty,
@@ -108,6 +116,7 @@ export const timelineContent = [
     "navYear": "1946",
   },
   {
+    "chapterIndex": getChapterIndex("wedding"),
     "date": "Sept 1947",
     "description": "With Joe's graduation from St. John's Law School, the happy couple were free to marry. Marie Tracy becomes Marie Irwin on September 13, 1947.",
     "imgSrc": wedding,
