729d5b01b42272e472617bfdd66fb0d807f063b4
tarinrickett <trickett96@gmail.com>
2020-12-20 09:29:04 -0800

    More scrapbook pages

--- src/constants/formattedChapters.js
+++ src/constants/formattedChapters.js
@@ -1,9 +1,10 @@
 export const formattedChapters = {
   "millerSisters": "Miller Sisters Dance Studio",
-  "tracySisters": "Tracy Sisters Dance School",
+  "tracySisters": "Tracy Sisters Studio",
   "amesSisters": "The Ames Sisters",
   "graduation": "Graduates High School",
   "marvelettes": "The Marvelettes",
+  "nedWayburn": "Ned Wayburn's School",
   "pathToTheRoxy": "Path to the Roxy",
   "roxyette": "Life at the Roxy",
   "fitModel": "Life at the Roxy",
--- src/constants/scrapbookContent.js
+++ src/constants/scrapbookContent.js
@@ -1,9 +1,10 @@
 const img = {
   'amesSisters': 'timeline/ames-sisters.jpg',
   'graduation': 'timeline/graduation.jpeg',
-  'marvelettes': 'timeline/marvelettes-pyramid.png',
+  'marvelettes': 'scrapbook/six-marvelettes.jpg',
   'millerSisters': 'scrapbook/miller-sisters.png',
-  'millerSistersRecital': 'timeline/miller-sisters-recital.jpg',
+  'millerSistersJuniorRecital': 'timeline/miller-sisters-recital.jpg',
+  'millerSistersLastRecital': 'scrapbook/miller-sisters-last-recital.jpg',
   'nedWayburn': 'scrapbook/ned-wayburn.jpg',
   'retirement': 'timeline/bar-across-the-street.png',
   'roxyette': 'scrapbook/ColorSpread.jpg',
@@ -13,7 +14,10 @@ const img = {
 }
 
 const audio = {
+  'amesSisters': 'and-i-was-an-ames-sister.mp3',
   'millerSisters': 'miller-sisters.mp3',
+  'millerSistersLastRecital': 'miller-sisters-last.mp3',
+  'nedWayburn': 'ned-wayburn.mp3',
   'tracySisters': 'tracy-sisters.mp3',
   'tracySistersProgram': 'tracy-sisters-program.mp3',
   'graduation': 'during-high-school.mp3'
@@ -35,13 +39,25 @@ export const scrapbookContent = [
     "date": "1938",
     "description": 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id imperdiet ante. Nulla at vulputate dui. Suspendisse commodo nunc massa, at gravida metus imperdiet id.',
     "chapter": "millerSisters",
-    "imgSrc": img.millerSistersRecital,
+    "imgSrc": img.millerSistersJuniorRecital,
     "title": "Junior Recital"
   },
   {
+    "audioSrc": audio.millerSistersLastRecital,
+    "caption": 'Marie is first from the left.',
+    "date": "1938",
+    "chapter": "millerSisters",
+    "imgSrc": img.millerSistersLastRecital,
+    "title": "Last Recital",
+    "transcription": "That must have been the last show I was in, with the Miller Sisters. Then I went on to Ned Wayburn's school..."
+  },
+  {
+    "audioSrc": audio.nedWayburn,
+    "caption": "Marie is on the top row, second from the right ('Me'). Her friend Florence stands next to her.",
     "chapter": "nedWayburn",
+    "date": "1936",
     "imgSrc": img.nedWayburn,
-    "title": "Placeholder: Ned Wayburn"
+    "transcription": "When I got enough money from teaching dancing, I went to a better school, I went to a dancing school in New York: Ned Wayburn. That's where I put my money. I used to go into the subway with my girlfriend, Florence Birkan. I wonder... I dated her brother. We used to go in the subway together on Saturdays. And there were triplets in that class; which I'll never be able to find now."
   },
   {
     "audioSrc": audio.tracySisters,
@@ -63,29 +79,40 @@ export const scrapbookContent = [
       "And this is the program... First recital: Patricia and Marie Tracy at 7:30PM. We rented a hall. We did baby acrobatics, we did waltz clog (that's tap dancing). Ann tap danced but... she knew everything, she was a pain in the neck so we threw her out. I don't think she liked it."
   },
   {
+    "audioSrc": audio.amesSisters,
+    "caption": "Marie replaced the Ames sister in the middle",
     "chapter": "amesSisters",
     "date": "March 1939",
     "imgSrc": img.amesSisters,
     "title": "On the road",
     "transcription":
-      "I did two years with the Ames Sisters and the Marvelettes. I was on the road for two years.",
+      "Now the Ames Sisters! That was when I was in high school. I went to another dancing studio. It was just acrobatic work and we had an Arabian teacher, Hansi. There were 5 real Ames Sisters and one of them, she got married and she got pregnant so they had all these bills to fill. They had been in the business a long time, the mother, the father were the managers, and the mother made the costumes. So they had to fill the bill that they had contracted for. So Hansi said take her; I was pretty much the same size. I looked something like some of them. So they put me in the costumes and taught me the routines and I was an Ames sister.",
   },
+  // {
+  //   "chapter": "amesSisters",
+  //   "date": "March 1939",
+  //   "imgSrc": img.amesSisters,
+  //   "title": "On the road",
+  //   "transcription":
+  //   "We played all kinds of different benefits and shows. The father had a big pack-up car, the top had a ... we were all jammed in there, a whole flock of us.",
+  // },
   {
     "audioSrc": audio.graduation,
     "chapter": "graduation",
     "date": "June 1940",
     "imgSrc": img.graduation,
     "title": "Newtown High School Class of 1940",
-    "transcription": 
+    "transcription":
       "During high school I was doing my homework at night while we were working the shows. I had my own dancing school on Saturdays, and then we would get gigs. The Masons would have a big banquet and they'd hire a couple of acts, or Jewish weddings would have a couple of acts..."
   },
   {
+    "caption": "From left to right: Marie, an original Ames Sister, Joan, a new girl, the second Ames Sister, and another new girl",
     "chapter": "marvelettes",
     "date": "1940",
     "imgSrc": img.marvelettes,
-    "title": "On the road",
+    "title": "Growing into a new group",
     "transcription":
-      "I did two years with the Ames Sisters and the Marvelettes. I was on the road for two years.",
+      "This is the Marvelettes now. Some of the Ames sisters are in here. These two are Ames sisters, and I was an Ames sister, and we picked up another two. This is the girl from Dobbs Ferry, Joan, I even remember her name.",
   },
   {
     "chapter": "roxyette",
