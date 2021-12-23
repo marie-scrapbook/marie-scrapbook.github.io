51f043a408e3f0a201774d1c3c09e9d5a292d5b2
trickett <trickett96@gmail.com>
2021-12-22 23:09:46 -0800

    Add story audio
    Revise transcriptions as needed

--- src/constants/storyContent.js
+++ src/constants/storyContent.js
@@ -17,12 +17,13 @@ export const storyContent = [
       "I know what I wanted to tell you. Is this on? I wanted to tell you that we went out every Saturday night in the audience. Saturday night house lights come on and we'd be in whatever outfit we were gonna wear that's cute, you, know. And we go out in the audience and we sold war bonds. Pledges. Every Saturday night. There were a lot of people buying them. I think there were $17.95 and then we'd charge $20, $25, I don't remember. But that's what we did on Saturday night. But when all the lights come on and all the girls came out in the aisles. The excitement, yeah. So that's what we did for the war."
   },
   {
+    "audioSrc": "stories/copa.mp3",
     "chapter": "roxyette",
     "imgSrc": "stories/copa.jpg", // https://jezebel.com/a-brief-twirl-through-eighty-years-at-the-copacabana-1843675986
     // "imgSrc": "", // https://www.nytimes.com/2020/06/19/nyregion/1957-yankees-brawl-copacabana-silvestri.html
     "title": "Copacabana Auditions",
     "transcription": 
-      "At one time 3 of us got together, we had no rehearsals so let's go try and they're hiring at the Latin Quarter. Copacabana I think it was. So we all went there at lunch times in full makeup– of course you can't take your makeup off between shows, it's hard to get off. Too long. And I was too tall and too short. I was too short for the show girls and they have what they call a Pony Line, which is a little kick routines, and I was too tall for that. Just something to do. Oh a lot of people looked at us. We have these big eyelashes on. There were a lot of nice clubs. Lot of nice clubs.",
+      "At one time, three of us got together, we got the giggles, we had no rehearsals, so let's go try at the– they're hiring at the Latin Quarter. So we went– Copacabana I think it was. So we go over there at lunch time. We all have full makeup on becauase you can't take your makeup off between shows, it's too hard to get on again. Too long. And I was too tall and too short. I was too short for the show girls and they have what they call a Pony Line, which is a little kick routine, and I was too tall for that. But we got the giggles anyhow.",
     "wiki": "Copacabana_(nightclub)"
   },
   {
@@ -33,37 +34,42 @@ export const storyContent = [
   },
   {
     "chapter": "tracys",
-    "title": "A Family Business",
+    "title": "All in the family",
     "transcription": 
       "And then my mother, my mother got sick. Trying to keep her busy. She had cancer. Anyhow, I said oh we have a heavy week this week, when I was in the costume department, we need seamstresses and not for the machine - sewing on snaps and buttons and things like that. And so my mother came in, 6 o'clock, a couple nights a week. And she had to get a union card also."
   },
   {
     "chapter": "tracys",
     "title": "Commuting",
+    "introAudioSrc": "stories/whistle.mp3",
     "introTranscription":
-      "They were good [my parents]. Maybe 2-3 nights a week they would meet me at the subway, 11:30pm at night. Sure they would rather go to bed, you know? Rather than have to walk up to the subway. A good walk- at dark. Daddy gave me a whistle that I wore around my neck. I had to whistle if somebody grabs me.",
+      "Many a night, maybe 2-3 nights a week they would meet me at the subway. At 11:30 at night. I’m sure they would rather be– go to bed, you know? Rather than have me walk up from the subway. [How far did you live from the subway?] Oh, a good walk. From here… past your house, yeah. But at dark… Daddy gave me a whistle that I wore around my neck. I was supposed to whistle if somebody grabbed me.",
+    "audioSrc": "stories/sunday-morning.mp3",
     "transcription":
-      "Sunday morning they would drive me in. Sunday morning they would have a movie first before the show. I wouldn't have to be in until noontime Sunday mornings. They would drive me in, all the way from Elmhurst. They cooperated."
+      "Sunday morning, again, they would drive me in. Sunday morning we had a movie first and then the show so I wouldn't have to be in until noontime. And a couple of nice Sunday mornings, they would drive me in, all the way from Elmhurst. They cooperated, yeah."
   },
   {
     "chapter": "tracys",
     "title": "The Sisters",
+    "audioSrc": "stories/sisters.mp3",
     "transcription":
-      "Well the war was on and Patricia wasn't home. She was married. And Ann was working at - not Sloan Kettering... that's where she met her husband, she was working at the hospital. She was working research. (Was Ann working at home?) Yes."
+      "Well, the war was on and Patricia wasn't home. Patricia was– [She was married by then?] Yeah, oh yeah, she was married. And Ann was working in– not Sloan Kettering... she was at the hospital– that's where she met her husband. She was working at the hospital in research. Yeah, she was working in research. [Was Ann living at home?] Yes."
   },
   {
     "chapter": "tracys",
     "imgSrc": "stories/willy-uniform.jpeg",
     "title": "Willy Tracy",
+    "audioSrc": "stories/willy.mp3",
     "transcription": 
-      "(What did your dad do?) He wasn't an accountant - he was a bookkeeper. He didn't have a degree in accounting. He worked for Texas Oil. And he did- he kept the books. He was in the Navy as a matter of fact. He was a yeoman in the Navy. Before he went in the Navy, he went to school he learned how to do - when you take depositions? What do you call that machine? There's a special machine, and he learned that. He was working in the courts. In Maine. Oh yeah. He was a maine-iac. Haha. Then he joined the Navy and came back to New York. They were married in 1918 - when was the war over? He came down in 17, the war was over in 18, they were married in 18. He was in uniform when they were married, so the war was still on. Because he was a yeoman, which is all book work, he was down at the Whitehall building at the end of New York, by Battery Park. He never went to the ocean."
+      "[What did your dad do?] He wasn't an accountant - he was a bookkeeper. He didn't get a degree in accounting. But he worked for Texas Oil. And he did- he kept the books. [He was trained in the Army or something, wasn’t he?] Not for that. He was in the Navy as a matter of fact. He was a yeoman in the Navy. But, before he went in the Navy, he went to school and he learned how to do– when you take depositions? What do you call that machine? [Stenographer?] Yeah something like that. There's a special machine. Yeah. And he learned that. He was working in the courts, up in Maine. [Oh ok. Court stenographer?] Then he joined the Navy and came down to New York. [When was that? World War 1?] Yeah. [Really?] Yeah. [He was in WWI?] Oh yeah. They were married in 1918 – when was the war over? [1917, 1918] Ok, so he came down in ‘17, the war was over in ‘18, I think. They were married in ‘18. He was in uniform when they were married, so the war was still on, yeah. [What kind of ship was he on? Do you know?] No, because he was a yeoman, which is all book work, he was down at the Whitehall building at the end of New York, down by Battery Place. [So he wasn’t out, on the ocean?] No, he never went to the ocean. [He didn’t go overseas?] No."
   },
   {
     "chapter": "tracys",
     "imgSrc": "stories/willy-mae-wedding.jpeg",
     "title": "Willy & Mae Tracy",
+    "audioSrc": "stories/willy-mae-wedding.mp3",
     "transcription":
-      "When they were married, it was during the flu epidemic of 1918. And my grandmother from Maine wouldn't come down because she was afraid of getting the flu. Everybody was wearing face masks. She was right. She wouldn't come to the wedding. They were dying like flies I guess.",
+      "When they were married, it was during the flu epidemic of 1918. And my grandmother from Maine wouldn't come down because she was afraid of getting the flu. Everybody was wearing face masks. She was right. She wouldn't come to the wedding. But they were dying like fleas I guess.",
   },
   {
     "chapter": "marieAndJoe",
@@ -76,6 +82,7 @@ export const storyContent = [
     "title": "He was like Stephen",
     "introTranscription": 
       "On my week off, my mother would invite him to dinner a couple nights a week. So we'd sit around, chatting. He had a lot of friends. He knew everybody. He was like Stephen - he had a lot of guys, he was a marshal. He was always busy doing something.",
+    "audioSrc": "stories/stephen.mp3",
     "transcription":
       "Everybody knew Joe. He was like Stephen - he knew everybody."
   },
@@ -94,48 +101,56 @@ export const storyContent = [
   {
     "chapter": "marieAndJoe",
     "title": "Friend of the Fireman",
+    "introAudioSrc": "stories/fireman.mp3",
     "introTranscription":
-      "He was a friend of the fireman. Every show a fireman had to sit backstage. That was the law, a NYC fireman, because the curtains were flammable. Or the paint or something. So you had to have a fireman. So the fireman was his neighbor, and his friend. It was Herbie's cousin, Herbie Houston’s cousin. “Hey Joe, you want to meet some pretty girls?” “Yeah sure, I got nothing else to do.” so he comes over there, he was sitting backstage. So... next thing, he's asking me for a date I said sure. Only a couple times. After that he didn't have to come backstage, he would call me up.",
+      "He was a friend of the fireman. Every show a fireman had to sit backstage. That was the law, a NYC fireman, because the curtains were flammable. Or the paint or something. So you had to have a fireman. So the fireman was his neighbor, and his friend. It was Herbie's cousin, Herbie Houston’s cousin. “Hey Joe, you want to meet some pretty girls?” “Yeah sure, I got nothing else to do.” So he comes over there, he was sitting backstage. So... next thing, he's asking me for a date I said sure.",
+    "audioSrc": "stories/elaine.mp3",
     "transcription":
-      "“Stage door Johnny”. I wasn't the first one! The first one was Elaine. Elaine. That didn't work out I guess, next thing I know he says “Wanna go out for a soda? A beer or something?” I say “Sure”",
+      "I wasn't the first one! The first one was Elaine. Elaine. That didn't work out I guess, next thing I know he says “Hey, wanna go out for a soda? Or whatever? A beer or something?” I say “Sure”",
   },
   {
     "chapter": "marieAndJoe",
     "title": "Jim Brannigan",
+    "audioSrc": "stories/jim.mp3",
     "transcription":
-      "He brought his friend Jim Brannigan, oh, that was a story. He brought his friend Jim Brannigan, “Come see my girl” so he comes to the show and that show we did kind of a circus act and the Roxy stage had kind of like a skirt on the end, on either side, and we were stationed at the end while an act was going on and I was standing out on the apex it was like standing here and here comes Jim Brannigan. He comes down the aisle and he's looking at me. Peering at me. And I'm trying to be theatrical, you know? Standing all - “Get-him-out-of-here”"
+      "Oh, that was a story. He brought his friend Jim Brannigan, “Come see my girl”. So he comes to the show and that show, we did some kind of a circus act, and the Roxy stage had kind of like a skirt on the end, on each side? And we were stationed at the end while an act was going on and I was standing out on the apex. And it was like standing here and here comes Jim Brannigan. He comes down the aisle and he's looking at me. Peering at me. And I'm trying to be theatrical, you know? Standing all - “Get-him-out-of-here”"
   },
   {
     "chapter": "marieAndJoe",
     "title": "Monsignor Irwin",
+    "audioSrc": "stories/uncle.mp3",
     "transcription":
-      "And then the time Joe brought his uncle, a Monsignor, Monsignor Irwin to see his girlfriend. Oh, I felt about this big. “Your mother wore tights”"
+      "And then the time Joe brought his uncle, a Monsignor! Monsignor Irwin to see his girlfriend. Oh, I felt about this big."
   },
   {
     "chapter": "after",
     "title": "Moving to Brooklyn",
     "transcription":
-      "That was right when the war finished. You couldn't find a place to live. So we were living in Joe's house, in his mother's house I probably would have stayed working because what was I gonna do there. You know? We didn't find - we didn't find any place to live in until... I guess it was just before Stephen was born. We found a flat, a place in Brooklyn. His cousin said– hey, I'm working with somebody in a two family house and they got a floor for rent. It was more than we could afford. But we were lucky to get it."
+      "And then– don't forget, that was right when the war finished. You couldn't find a place to live. So we were living in Joe's house, in his mother's house. I probably would have stayed working because what am I gonna do there? You know? We didn't find - we didn't find any place to live in until... I guess it was just before Stephen was born. We found a flat, a place in Brooklyn. His cousin said– hey, I'm working with somebody in a two family house and they got a floor for rent. It was more than we could afford. But we were lucky to get it."
   },
   {
     "chapter": "after",
     "title": "Back to Queens",
+    "audioSrc": "stories/queens.mp3",
     "transcription":
-      "But then I was only there 6 months and then Daddy called me home because mother was dying. So we moved home. So that's how we got to Elmhurst. My mother died 10 days later. But we stayed there until Michael was born because Joe said to Daddy - my father — we can't stay, we need more space. Because we were living in the basement, we were living here, there was a room there.. it was all room all over the place. So my father begged us. If you stay- ... Ann was there"
+      "But then I was only there 6 months and then Daddy called me home because mother was dying. So we moved home. So that's how we got to Elmhurst. My mother died 10 days later. But we stayed there until Michael was born because Joe said to Daddy - my father — we can't stay, we need more space. Because we were living in the basement, we were living here, there was a room there.. it was all wrong. All over the place. So my father begged us. He said if you stay- Ann was there too– if you stay, I'll get rid of the tenants and you'll have the whole place. So we stayed. And we stayed there until my father died. And then we moved to Garden City Park."
   },
   {
     "chapter": "after",
     "title": "Mother of the year",
+    "introAudioSrc": "stories/home.mp3",
     "introTranscription":
-      "[Re: the Chinese restaurant] It was like home. It was just like home",
+      "[Re: the Chinese restaurant] Oh, it was like home. It was just like home",
+    "audioSrc": "stories/mother-of-the-year.mp3",
     "transcription":
       "And the owner was a young man. Not much older than we were. And he got married and had a little boy and he called him Kevin. Kevin Chang. We'd go back there once in a while and we'd get a babysitter and he'd say “Here comes the mother of the year” – I always had a new baby."
   },
   {
     "title": "Now you know",
     // "imgSrc": "stories/Momma.png",
+    "introAudioSrc": "stories/thank-you.mp3",
     "introTranscription":
-      "Well this was very nice, thank you. I never would have sat down and thought about all these good times that I had. I did enjoy it very much. You know, it sounds like work but it wasn't work when you like what you're doing. Whatever you're doing- if you like it, You got it.",
+      "Well this was very nice, thank you. I would never have sat down and thought about all these good times that I had. I did enjoy it very much. You know, it sounds like work but it wasn't work when you like what you're doing. Whatever you're doing- if you like it, you got it.",
     "transcription":
       "Now you know my life story. There it is! It's not very exciting but it was when I was doing it. I had a great time."
   }
